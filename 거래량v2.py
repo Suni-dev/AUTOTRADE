53a689ec3588d160825927d6537cd90c4cc0b795
Suni <114858534+Suni-dev@users.noreply.github.com>
2023-11-08 06:14:07 +0900

    Update 거래량v2.py atr 매수로직 변경

--- 거래량v2.py
+++ 거래량v2.py
@@ -4,8 +4,8 @@ import datetime
 import requests
 import pandas as pd
 
-access = ""
-secret = ""
+access = "bRwAU1P0CIVkBQWWMxJ6TGLa56TVidXz6TvPWQji"
+secret = "fI1yNBxDSk6Onaa1bnE7LV7IQIx1PO3pJjSrhyvZ"
 
 
 def get_moving_average_volume(ticker, interval, count):
@@ -67,7 +67,7 @@ upbit = pyupbit.Upbit(access, secret)
 print("자동매매 시작")
 
 MAX_INVESTMENT_COUNT = 6  # 최대 투자 코인 개수
-INVESTMENT_RATIO = 0.1  # 총 자산 대비 투자 비율
+INVESTMENT_RATIO = 0.166  # 총 자산 대비 투자 비율
 STOP_LOSS_RATIO = 0.97  # 손절 비율 (3% 손실)
 TAKE_PROFIT_RATIO = 1.03  # 익절 비율 (3% 이익)
 MINIMUM_KRW_ORDER = 5000  # 최소 KRW 주문 금액
@@ -91,13 +91,18 @@ while True:
                     (current_price - info["price"]) / info["price"] * 100
                 )  # 평가손익 계산
                 message += (
-                    f"{ticker}: 매수 가격 - {info['price']} BTC, 현재 가격 - {current_price} BTC, "
-                    f"목표 매도가 - {sell_price} BTC, 평가손익 - {profit_or_loss:.2f}%\n"
+                    f"*{ticker}*\n"
+                    f"매수 가격: `{info['price']:,}` KRW\n"  # 천 단위 구분자 추가
+                    f"현재 가격: `{current_price:,}` KRW\n"  # 천 단위 구분자 추가
+                    f"목표 매도가: `{sell_price:,}` KRW\n"  # 천 단위 구분자 추가
+                    f"평가손익: `{profit_or_loss:.2f}%`\n\n"
                 )
             send_telegram_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, message)
         else:
             send_telegram_message(
-                TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, "현재 보유 중인 종목이 없습니다."
+                TELEGRAM_BOT_TOKEN,
+                TELEGRAM_CHAT_ID,
+                "현재 보유 중인 종목이 없습니다. 매수 타점을 잡는 중입니다.",
             )
 
         krw_balance = get_balance("KRW")
@@ -113,36 +118,33 @@ while True:
             if avg_volume is None or last_volume is None or atr is None:
                 continue
 
-            volume_ratio = last_volume / avg_volume  # 마지막 1분 거래량 / 5분 이동평균 거래량
-            dynamic_take_profit_ratio = 1 + (atr / current_price)  # 동적 익절 비율 계산
+            volume_ratio = last_volume / avg_volume
+            dynamic_take_profit_ratio = 1 + (atr / current_price)
+            dynamic_stop_loss_ratio = 1 - (atr / current_price)
 
-            # 투자 개수가 MAX_INVESTMENT_COUNT보다 작을 때만 매수를 진행
+            # 매수 조건 검사 및 매수 처리
             if (
                 volume_ratio > VOLUME_SURGE_THRESHOLD
                 and num_investments < MAX_INVESTMENT_COUNT
+                and krw_balance > MINIMUM_KRW_ORDER
+                and investment_limit > MINIMUM_KRW_ORDER
             ):
-                if (
-                    krw_balance > MINIMUM_KRW_ORDER
-                    and investment_limit > MINIMUM_KRW_ORDER
-                ):
-                    buy_result = upbit.buy_market_order(ticker, investment_limit)
-                    invested[ticker] = {
-                        "price": current_price,
-                        "volume": investment_limit / current_price,
-                    }
-                    num_investments += 1
-                    message = f"{ticker}에 투자했습니다: {buy_result}"
-                    print(message)
-                    send_telegram_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, message)
+                buy_result = upbit.buy_market_order(ticker, investment_limit)
+                invested[ticker] = {
+                    "price": current_price,
+                    "volume": investment_limit / current_price,
+                    "stop_loss": current_price * dynamic_stop_loss_ratio,
+                    "take_profit": current_price * dynamic_take_profit_ratio,
+                }
+                num_investments += 1
+                message = f"{ticker}에 투자했습니다: {buy_result}"
+                print(message)
+                send_telegram_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, message)
 
             # 매도 조건 검사 및 매도 처리
             if ticker in invested:
                 investment_info = invested[ticker]
-                # 동적 익절 비율을 사용하여 매도 조건 검사
-                if (
-                    current_price
-                    >= investment_info["price"] * dynamic_take_profit_ratio
-                ):
+                if current_price >= investment_info["take_profit"]:
                     sell_result = upbit.sell_market_order(
                         ticker, investment_info["volume"]
                     )
@@ -150,7 +152,7 @@ while True:
                     print(message)
                     send_telegram_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, message)
                     del invested[ticker]
-                elif current_price <= investment_info["price"] * STOP_LOSS_RATIO:
+                elif current_price <= investment_info["stop_loss"]:
                     sell_result = upbit.sell_market_order(
                         ticker, investment_info["volume"]
                     )
@@ -163,3 +165,4 @@ while True:
         error_message = f"에러 발생: {e}"
         print(error_message)
         send_telegram_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, error_message)
+        time.sleep(1)
